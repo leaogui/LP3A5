4e39eac81b733666268b80efe6c6d621ac82f17a
leaogui <guiladriero@gmail.com>
2020-03-04 10:28:51 -0300

    consertando

--- Aula2/ex1/src/ex1/Teste.java
+++ Aula2/ex1/src/ex1/Teste.java
@@ -4,26 +4,26 @@ public class Teste implements Runnable{
 
 	
 	int x;
-	String text;
-	int mili;
+	String txt;
+	int m;
 	
 	public Teste(String txt, int m) {
-		this.text = text;
-		this.mili = mili;
+		this.txt = txt;
+		this.m = m;
 	}
 	
 	@Override
 	public void run() {
 		try {
 			while(true) {
-				System.out.println(text + x);
+				System.out.println(txt + x);
 				x += 1;
-				Thread.sleep(mili);
+				Thread.sleep(m);
 			}
 		} catch (InterruptedException e){
 			System.out.println("\n" + Thread.currentThread().getName() + " interrompida.");
 			System.out.println("INTERROMPIDO flag: " + Thread.currentThread().isInterrupted());
 		}
-		System.out.println("Thread " + Thread.currentThread().getName() + " Acabou.");
+		System.out.println("Thread " + Thread.currentThread().getName() + " acabou.");
 	}
 }
